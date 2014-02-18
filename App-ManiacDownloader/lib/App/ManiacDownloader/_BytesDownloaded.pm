d0e8590a29deaba671b409fb6248b34120a96e05
Shlomi Fish <shlomif@shlomifish.org>
2014-02-18 21:49:09 +0200

    Add the stale_checkpoints feature.

--- App-ManiacDownloader/lib/App/ManiacDownloader/_BytesDownloaded.pm
+++ App-ManiacDownloader/lib/App/ManiacDownloader/_BytesDownloaded.pm
@@ -10,6 +10,9 @@ use List::Util qw/min/;
 has ['_bytes_dled', '_bytes_dled_last_timer'] =>
     (isa => 'Int', is => 'rw', default => sub { return 0;});
 
+has '_stale_checkpoints_count' => (isa => 'Int', is => 'rw',
+    default => sub { return 0;});
+
 sub _add
 {
     my ($self, $num_written) = @_;
@@ -28,11 +31,27 @@ sub _total_downloaded
     return $self->_bytes_dled;
 }
 
+sub _were_stale_checkpoints_exceeded
+{
+    my ($self, $MAX_COUNT) = @_;
+
+    return ($self->_stale_checkpoints_count >= $MAX_COUNT);
+}
+
 sub _flush_and_report
 {
     my $self = shift;
 
     my $difference = $self->_bytes_dled - $self->_bytes_dled_last_timer;
+
+    if ($difference > 0)
+    {
+        $self->_stale_checkpoints_count(0);
+    }
+    else
+    {
+        $self->_stale_checkpoints_count($self->_stale_checkpoints_count + 1);
+    }
 
     $self->_bytes_dled_last_timer($self->_bytes_dled);
 
