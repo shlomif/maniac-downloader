65c22aa80e31efb20bca8a99c208f45aa5c41539
Shlomi Fish <shlomif@shlomifish.org>
2015-01-28 13:08:42 +0200

    Bump version to 0.0.11.

--- App-ManiacDownloader/lib/App/ManiacDownloader.pm
+++ App-ManiacDownloader/lib/App/ManiacDownloader.pm
@@ -20,7 +20,7 @@ use JSON::MaybeXS qw(decode_json encode_json);
 use App::ManiacDownloader::_SegmentTask;
 use App::ManiacDownloader::_BytesDownloaded;
 
-our $VERSION = '0.0.10';
+our $VERSION = '0.0.11';
 
 my $DEFAULT_NUM_CONNECTIONS = 4;
 my $NUM_CONN_BYTES_THRESHOLD = 4_096 * 2;
